2a246c3391e23a6063309f7a9c9ed3de06719b71
MaxPea <max.pean807@gmail.com>
2023-01-21 17:20:05 +0100

    21012023

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,12 @@ function myMap() {
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
+
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});  
